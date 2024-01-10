2d0fb69b889a272c899c7b10f4a07c024dc99116
FaqodAqomaddin <115222095+FaqodAqomaddin@users.noreply.github.com>
2024-01-10 22:50:48 +0700

    membuat navigasi untuk ke halaman detail

--- app/src/main/java/com/example/uaspam/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/uaspam/navigasi/PengelolaHalaman.kt
@@ -3,11 +3,15 @@ package com.example.uaspam.navigasi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.uaspam.ui.Add.AddScreen
 import com.example.uaspam.ui.Add.DestinasiEntry
+import com.example.uaspam.ui.Detail.DetailMotorDestination
+import com.example.uaspam.ui.Detail.DetailScreen
 import com.example.uaspam.ui.Home.DestinasiHomeMotor
 import com.example.uaspam.ui.Home.HomeScreen
 
@@ -19,11 +23,28 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
         startDestination = DestinasiHomeMotor.route,
         modifier = Modifier
     ) {
-        composable(DestinasiHomeMotor.route){
-            HomeScreen(navigateToItemEntry = { navController.navigate(DestinasiEntry.route) })
+        composable(DestinasiHomeMotor.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                onDetailClick = { itemId ->
+                    navController.navigate("${DetailMotorDestination.route}/$itemId")
+                    println("itemId: $itemId")
+                })
         }
-        composable(DestinasiEntry.route){
-            AddScreen(navigateBack = { navController.popBackStack()})
+        composable(DestinasiEntry.route) {
+            AddScreen(navigateBack = { navController.popBackStack() })
+        }
+        composable(route = DetailMotorDestination.routeWithArgs,
+            arguments = listOf(navArgument(DetailMotorDestination.MotorId) {
+                type = NavType.StringType
+            })
+        ) { backStackEntry ->
+            val kontakId = backStackEntry.arguments?.getString(DetailMotorDestination.MotorId)
+            kontakId?.let {
+                DetailScreen(
+                    navigateToEditItem = {},
+                    navigateBack = { navController.popBackStack() })
+            }
         }
     }
 }
