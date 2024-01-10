264f5f70f4515079e4477cdaa8ff28657f3d9e87
nabielmakareem <nabielmakareem10@gmail.com>
2024-01-11 00:00:17 +0700

    menambah tampilan detail

---
+++ app/src/main/java/com/example/uaspam/ui/Detail/DetailPemilikScreen.kt
@@ -0,0 +1,203 @@
+package com.example.uaspam.ui.Detail
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.uaspam.model.Pemilik
+import com.example.uaspam.ui.DetailUIStatePemilik
+import com.example.uaspam.ui.PenyediaViewModel
+import com.example.uaspam.ui.toPemilik
+import kotlinx.coroutines.launch
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.uaspam.navigasi.DestinasiNavigasi
+import com.example.uaspam.ui.DealerTopAppBar
+
+object DetailPemilikDestination : DestinasiNavigasi {
+    override val route = "item_details"
+    override val titleRes = "Detail Pemilik"
+    const val pemilikId = "itemId"
+    val routeWithArgs = "$route/{$pemilikId}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailScreen(
+    navigateToEditItem: (String) -> Unit,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailPemilikViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val uiState = viewModel.pemilikuiState.collectAsState()
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        topBar = {
+            DealerTopAppBar(
+                title = DetailPemilikDestination.titleRes,
+                canNavigateBack = true,
+                navigateUp = navigateBack
+            )
+        }, floatingActionButton = {
+            FloatingActionButton(
+                onClick = { navigateToEditItem(uiState.value.addEventPemilik.id) },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "",
+                )
+            }
+        }, modifier = modifier
+    ) { innerPadding ->
+        ItemDetailsBodyPemilik(
+            detailUIStatepemilik = uiState.value,
+            onDelete = {
+                coroutineScope.launch {
+                    viewModel.deleteKontak()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState()),
+
+            )
+    }
+}
+
+@Composable
+private fun ItemDetailsBodyPemilik(
+    detailUIStatepemilik: DetailUIStatePemilik,
+    onDelete: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.padding(12.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+        ItemDetailsPemilik(
+            pemilik  = detailUIStatepemilik.addEventPemilik.toPemilik(), modifier = Modifier.fillMaxWidth()
+        )
+
+        OutlinedButton(
+            onClick = { deleteConfirmationRequired = true },
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Delete")
+        }
+        if (deleteConfirmationRequired) {
+            DeleteConfirmationDialog(
+                onDeleteConfirm = {
+                    deleteConfirmationRequired = false
+                    onDelete()
+                },
+                onDeleteCancel = { deleteConfirmationRequired = false },
+                modifier = Modifier.padding(12.dp)
+            )
+        }
+    }
+}
+
+
+@Composable
+fun ItemDetailsPemilik(
+    pemilik: Pemilik, modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier, colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            ItemDetailsRowPemilik(
+                labelResID ="Nama",
+                itemDetail = pemilik.nama,
+                modifier = Modifier.padding(
+                    horizontal = 12.dp
+                )
+            )
+            ItemDetailsRowPemilik(
+                labelResID = "Alamat",
+                itemDetail = pemilik.alamat,
+                modifier = Modifier.padding(
+                    horizontal = 12.dp
+                )
+            )
+            ItemDetailsRowPemilik(
+                labelResID ="No. Telpon",
+                itemDetail = pemilik.telpon,
+                modifier = Modifier.padding(
+                    horizontal = 12.dp
+                )
+            )
+        }
+
+    }
+}
+@Composable
+private fun ItemDetailsRowPemilik(
+    labelResID: String, itemDetail: String, modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier) {
+        Text(text = labelResID, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.weight(1f))
+        Text(text = itemDetail, fontWeight = FontWeight.Bold)
+    }
+}
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit, onDeleteCancel: () -> Unit, modifier: Modifier = Modifier
+) {
+    AlertDialog(onDismissRequest = { /* Do nothing */ },
+        title = { Text("Are you sure") },
+        text = { Text("Delete") },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = "No")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Yes")
+            }
+        })
+}
