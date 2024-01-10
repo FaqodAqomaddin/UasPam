4691550427d41a8e21b2e185d819735c8958f722
nabielmakareem <nabielmakareem10@gmail.com>
2024-01-10 23:09:24 +0700

    menambah viewmodel untuk edit motor

---
+++ app/src/main/java/com/example/uaspam/ui/Edit/EditMotorViewModel.kt
@@ -0,0 +1,46 @@
+package com.example.uaspam.ui.Edit
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.uaspam.data.MotorRepository
+import com.example.uaspam.ui.AddEvent
+import com.example.uaspam.ui.AddUIState
+import com.example.uaspam.ui.toMotor
+import com.example.uaspam.ui.toUIStateMotor
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+class EditMotorViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val motorrepository: MotorRepository
+) : ViewModel() {
+
+    var kontakUiState by mutableStateOf(AddUIState())
+        private set
+
+    private val motorId: String = checkNotNull(savedStateHandle[EditMotorDestination.MotorId])
+
+    init {
+        viewModelScope.launch {
+            kontakUiState =
+                motorrepository.getMotorById(motorId)
+                    .filterNotNull()
+                    .first()
+                    .toUIStateMotor()
+        }
+    }
+
+    fun updateUIState(addEvent: AddEvent) {
+        kontakUiState = kontakUiState.copy(addEvent = addEvent)
+    }
+
+    suspend fun updateKontak() {
+        motorrepository.update(kontakUiState.addEvent.toMotor())
+
+    }
+}
