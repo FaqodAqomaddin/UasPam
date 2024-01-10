3c17bab8b48451a164888d4a890bda20a0bf522a
nabielmakareem <nabielmakareem10@gmail.com>
2024-01-11 00:00:44 +0700

    menambah viewmodel detail

---
+++ app/src/main/java/com/example/uaspam/ui/Detail/DetailPemilikViewModel.kt
@@ -0,0 +1,43 @@
+package com.example.uaspam.ui.Detail
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.uaspam.data.PemilikRepository
+import com.example.uaspam.ui.DetailUIStatePemilik
+import com.example.uaspam.ui.toDetailPemilik
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class DetailPemilikViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val pemilikrepository: PemilikRepository
+) : ViewModel() {
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+    val pemilikId: String = checkNotNull(savedStateHandle[DetailPemilikDestination.kontakId])
+
+    val pemilikuiState: StateFlow<DetailUIStatePemilik> =
+        pemilikrepository.getKontakById(pemilikId)
+            .filterNotNull()
+            .map {
+                DetailUIStatePemilik(addEventPemilik = it.toDetailPemilik())
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = DetailUIStatePemilik()
+            )
+
+    suspend fun deleteKontak() {
+        pemilikrepository.delete(pemilikId)
+
+    }
+
+
+}
